14ce2c9660f0c945304d44c89ba99da6b3c0806a
oggn <99378862+oggn@users.noreply.github.com>
2024-08-05 21:11:03 +0900

    상품리스트(메인페이지) 페이지네이션 적용

--- review_spot_frontend/src/components/main/productListComponent.tsx
+++ review_spot_frontend/src/components/main/productListComponent.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import Pagination from "../common/pagenationComponent";
 export default function ProductList() {
   const products = [
     {
@@ -38,56 +41,128 @@ export default function ProductList() {
       area: "부산",
       category: "category",
     },
+    {
+      id: 5,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
+    {
+      id: 6,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
+    {
+      id: 7,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
+    {
+      id: 8,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
+    {
+      id: 9,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
+    {
+      id: 10,
+      name: "Evan Williams",
+      imgPath: "/evanWilliams.jpg",
+      alcohol: 10,
+      capacity: 5,
+      area: "부산",
+      category: "category",
+    },
   ];
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9; // 한 페이지에 보여줄 상품 수
+  // 페이지에 따른 데이터 계산
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
 
+  // 페이지 이동 함수
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   return (
-    <div className="grid grid-cols-3 w-full gap-5 p-5">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="flex flex-row items-center border border-sky-500 rounded-lg overflow-hidden ease duration-300 hover:-translate-y-2 w-full"
-        >
-          <div className="relative w-48 h-48 flex-shrink-0">
-            <Image
-              src={product.imgPath}
-              alt={product.name}
-              layout="fill"
-              objectFit="cover"
-            />
+    <div className="w-full flex flex-col">
+      <div className="grid grid-cols-3 w-full gap-5 p-5">
+        {currentItems.map((product) => (
+          <div
+            key={product.id}
+            className="flex flex-row items-center border border-sky-500 rounded-lg overflow-hidden ease duration-300 hover:-translate-y-2 w-full"
+          >
+            <div className="relative w-48 h-48 flex-shrink-0">
+              <Image
+                src={product.imgPath}
+                alt={product.name}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <div className="w-full h-full pt-5 pb-4 flex flex-col justify-between items-start">
+              <div className="flex">
+                <p className="px-5 text-left text-3xl flex-grow">
+                  {product.name}
+                </p>
+              </div>
+              <div className="flex">
+                <p className="px-5 text-left text-base flex-grow">
+                  알코올 : {product.alcohol}
+                </p>
+                <p className="px-5 text-left text-base flex-grow">
+                  category : {product.category}
+                </p>
+              </div>
+              <div className="flex">
+                <p className="px-5 text-left text-base flex-grow">
+                  capacity : {product.capacity}
+                </p>
+                <p className="px-5 text-left text-base flex-grow">
+                  지역 : {product.area}
+                </p>
+              </div>
+              <div className="w-full text-end">
+                <Link
+                  href={"/productInfo"}
+                  className="text-sky-500 hover:bg-sky-500 hover:text-white rounded-lg text-lg px-5"
+                >
+                  상세보기
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="w-full h-full pt-5 pb-4 flex flex-col justify-between items-start">
-            <div className="flex">
-              <p className="px-5 text-left text-3xl flex-grow">
-                {product.name}
-              </p>
-            </div>
-            <div className="flex">
-              <p className="px-5 text-left text-base flex-grow">
-                알코올 : {product.alcohol}
-              </p>
-              <p className="px-5 text-left text-base flex-grow">
-                category : {product.category}
-              </p>
-            </div>
-            <div className="flex">
-              <p className="px-5 text-left text-base flex-grow">
-                capacity : {product.capacity}
-              </p>
-              <p className="px-5 text-left text-base flex-grow">
-                지역 : {product.area}
-              </p>
-            </div>
-            <div className="w-full text-end">
-              <Link
-                href={"/productInfo"}
-                className="text-sky-500 hover:bg-sky-500 hover:text-white rounded-lg text-lg px-5"
-              >
-                상세보기
-              </Link>
-            </div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Pagination
+          itemsPerPage={itemsPerPage}
+          totalItems={products.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
     </div>
   );
 }
