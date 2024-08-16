5017a110b452e358bf40162fdaebb3c6185149d9
kai <m01056994380@gmail.com>
2024-08-16 11:26:25 +0900

    Refactor: 소스 리팩토링(#7)

--- review_spot_backend/product/urls.py
+++ review_spot_backend/product/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from product import views
 
 urlpatterns = [
-    path('list/', views.ProductList.as_view(), name='index')
+    path('list/', views.ProductList.as_view())
 ]
