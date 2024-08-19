734aac7563e378bdcdeb8d80490e9a226d05fb00
kai <m01056994380@gmail.com>
2024-08-19 13:58:25 +0900

    Remove: 루트 디렉토리에 필요없는 파일 삭제

--- review_spot_backend/product/models.py
+++ review_spot_backend/product/models.py
@@ -3,12 +3,29 @@ from django.db import models
 # Create your models here.
 
 
-# 양주, 술안주 등 리뷰 대상 모델
+# 양주, 술안주 등 리뷰 대상 제품 모델
 class Product(models.Model):
     name = models.CharField(
-        help_text='제품명'
+        verbose_name='제품명'
     )
-    description = models.CharField(
-        help_text='제품 설명',
+
+    imgPath = models.CharField(
+        verbose_name='제품 이미지 경로'
     )
+
+    alcohol = models.IntegerField(
+        verbose_name='도수'
+    )
+
+    capacity = models.IntegerField(
+        verbose_name='양주 용량'
+    )
+
+    area = models.CharField(
+        verbose_name='양주 생산지'
+    )
+
+    # category = models.ForeignKey(
+    #     verbose_name='제품 종류'
+    # )
 
