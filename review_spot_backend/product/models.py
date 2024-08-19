5950a8795adc3acd4b55c866fb0339024c86a45c
kai <m01056994380@gmail.com>
2024-08-19 17:08:43 +0900

    Setting: 모델 설정(#7)

--- review_spot_backend/product/models.py
+++ review_spot_backend/product/models.py
@@ -6,26 +6,48 @@ from django.db import models
 # 양주, 술안주 등 리뷰 대상 제품 모델
 class Product(models.Model):
     name = models.CharField(
-        verbose_name='제품명'
+        verbose_name='제품명',
     )
 
     imgPath = models.CharField(
-        verbose_name='제품 이미지 경로'
+        verbose_name='제품 이미지 경로',
+        blank=True,
+        null=True,
     )
 
-    alcohol = models.IntegerField(
-        verbose_name='도수'
+    product_info = models.JSONField(
+        verbose_name='제품 정보 관련 데이터',
+        default=dict,
+        blank=True,
+        null=True,
     )
 
-    capacity = models.IntegerField(
-        verbose_name='양주 용량'
+    created = models.DateTimeField(
+        verbose_name='생성일시',
+        auto_now_add=True,
     )
 
-    area = models.CharField(
-        verbose_name='양주 생산지'
+    updated = models.DateTimeField(
+        verbose_name='수정일시',
+        auto_now=True,
     )
 
-    # category = models.ForeignKey(
-    #     verbose_name='제품 종류'
+    category = models.ForeignKey(
+        'category.Category',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    # alcohol = models.IntegerField(
+    #     verbose_name='도수'
     # )
+    #
+    # capacity = models.IntegerField(
+    #     verbose_name='양주 용량'
+    # )
+    #
+    # area = models.CharField(
+    #     verbose_name='양주 생산지'
+    # )
+
 
