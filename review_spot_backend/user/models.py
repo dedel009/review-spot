18d7971cd82e719ff1d34892c42a6a8537224070
doyun <dedel009@ronfic.com>
2024-09-19 23:01:30 +0900

    Fix:마이그레이션 진행

--- review_spot_backend/user/models.py
+++ review_spot_backend/user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -32,13 +33,37 @@ class CustomUserManager(BaseUserManager):
 
 # 커스텀 유저 모델
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=150, unique=True, verbose_name='유저 아이디')
-    external_uuid = models.UUIDField(unique=True, default=uuid.uuid4, verbose_name='통신용 uuid')
-    name = models.CharField(max_length=30, null=False, verbose_name='유저 이름')
-    email = models.EmailField(blank=True, verbose_name='유저 이메일')
-    is_active = models.BooleanField(default=True, verbose_name='유저 사용 유무')
-    is_staff = models.BooleanField(default=False, verbose_name='유저 관리자 유무')
-    date_joined = models.DateTimeField(auto_now_add=True, verbose_name='생성 일자')
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='유저 아이디',
+    )
+    external_uuid = models.UUIDField(
+        unique=True,
+        default=uuid.uuid4,
+        verbose_name='통신용 uuid',
+    )
+    name = models.CharField(
+        max_length=30,
+        null=False,
+        verbose_name='유저 이름',
+    )
+    email = models.EmailField(
+        blank=True,
+        verbose_name='유저 이메일',
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='유저 사용 유무',
+    )
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name='유저 관리자 유무',
+    )
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='생성 일자',
+    )
 
     # Django의 ORM(Object-Relational Mapping)에서 모델 매니저를 지정
     objects = CustomUserManager()
