ff42dbb7d9f88b9a8809849e0fb711b5d185bed7
oggn <99378862+oggn@users.noreply.github.com>
2024-11-27 22:46:46 +0900

    ㅇㄴㅇ

--- review_spot_frontend/src/app/api/reviews/get_list/route.ts
+++ review_spot_frontend/src/app/api/reviews/get_list/route.ts
@@ -1,15 +1,14 @@
-// app/api/reviews/get_list/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
 
+// TLS 인증서 경고 무시 (개발 환경에서만 사용)
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export async function GET(req: NextRequest) {
   try {
     // Extract page and limit from the query parameters
-    const { searchParams } = new URL(req.url);
-    const page = parseInt(searchParams.get("page_num") || "1", 10);
-    const limit = parseInt(searchParams.get("display") || "10", 10);
+    const url = new URL(req.url); // URL 객체를 사용하여 쿼리 파라미터 추출
+    const page = parseInt(url.searchParams.get("page_num") || "1", 10);
+    const limit = parseInt(url.searchParams.get("display") || "10", 10);
 
     // Make a request to the Django server with page and limit as query parameters
     const res = await fetch(
